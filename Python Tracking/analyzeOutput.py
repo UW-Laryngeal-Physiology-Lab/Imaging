547d34df946c274a70b1cc3409d1a15872a590e5
Ben Wurster <55667072+BWurster@users.noreply.github.com>
2021-07-20 14:24:33 -0500

    ineffective curve fit

--- Python Tracking/analyzeOutput.py
+++ Python Tracking/analyzeOutput.py
@@ -11,11 +11,35 @@
 # necessary imports
 from src import *
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('./assets/motionData.csv')
+"data variable is indexed data[frame #, point #]"
 data = df.to_numpy()
+graphs.basicPlot(data)
 
+from src.smooth import smoothData
+smoothData()
+df_smooth = pd.read_csv('./assets/motionDataSmooth.csv')
+"data variable is indexed data[frame #, point #]"
+data_smooth = df_smooth.to_numpy()
+graphs.basicPlot(data_smooth)
+
+input("<Enter>")
+
+'''
+maxIndices is a 2D list with the first dimension being the point and the next 
+being the index; maxIndices[point][maxVals]
+'''
 maxIndices = process.getMaxIndices(data)
 
-graphs.plotMotion(data, maxIndices)
-# graphs.plotAveraged(averagedData)
+# derivative call
+deltaData = np.empty((data.shape[0] - 1, data.shape[1]))
+for i in range(data.shape[1]):
+    deltaData[:, i] = np.diff(data[:,i])
+derivCurves = graphs.fitSingleCurves(deltaData)
+
+singleCurves = graphs.fitSingleCurves(data, derivCurves) 
+    # can add '=None' to remove derivative function
+graphs.plotMotion(data, maxIndices, singleCurves)
